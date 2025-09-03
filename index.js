ecb3dddb562d124e3aaba57f01868dd68df6c547
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2025-09-03 08:16:19 +0530

    change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const MONGODB_URI = process.env.MONGODB_URI 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/student-grades';
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
